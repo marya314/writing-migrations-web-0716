aa2a64ace36a45765b38d95eb1ea4842617e8ef7
marya314 <mary.boyd314@gmail.com>
2016-08-08 19:23:21 -0400

    Completed lab - added additional classes for migration

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,17 @@
 class CreateStudents < ActiveRecord::Migration
+
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        )
+        SQL
+
+
+    def change
+        create_table :students do |s|
+            s.string :name
+        end
+    end
+
 end
